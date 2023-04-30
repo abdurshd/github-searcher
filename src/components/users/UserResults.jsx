b6c2c5bcbb583c852f1305a669d6f0cce5c29c2f
aburashidakbarov <abdurshd@icloud.com>
2023-04-30 23:59:21 +0900

    added search component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,31 +1,22 @@
-import {useContext, useEffect} from 'react';
+import {useContext} from 'react';
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
 import GithubContext from '../../context/github/GithubContext';
 
 const UserResults = () => {
-const {users, loading, fetchUsers} = useContext(GithubContext);
+const {users, loading} = useContext(GithubContext);
 
-  useEffect(() => {
-     fetchUsers()  
-   }, []);
-
-
-
-  return (
-    <div>
-      {loading ? 
-      (<Spinner/>
-      ) : (
-<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-      {Array.isArray(users) && users.map((user) => (
-        <UserItem key={user.id} user={user}/>))
-        }
-        
-    </div>
-      ) }
-    </div>
-  )
+   if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  } else {
+    return <Spinner />
+  }
 }
 
 export default UserResults
