0cc8d261f041b1aaa12bdab231261dd0a33ab5c4
aburashidakbarov <abdurshd@icloud.com>
2023-05-02 19:24:14 +0900

    created GithubActions and cleaned up GithubContext

---
+++ src/context/github/GithubActions.js
@@ -0,0 +1,54 @@
+ import axios from 'axios';
+ 
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+//searchUser
+export const searchUsers =  async (text) => {
+    const params = new URLSearchParams({
+      q: text
+    })
+   
+    const res = await fetch(`${GITHUB_URL}/search/users?${params}`,{
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+  
+  const {items} = await res.json()
+  
+ return items
+  }
+
+  //getUser
+  export const getUser =  async (login) => {
+      const res = await fetch(`${GITHUB_URL}/users/${login}`,{
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+  if (res.status === 404) {
+    window.location = '/notfound'
+  } else {
+  const data = await res.json()
+return data
+  }
+  }
+  
+  //Get user repos
+  export const getUserRepos =  async (login) => {
+    const params = new URLSearchParams({
+      sort: 'created' ,
+      per_page: 10
+    })
+
+    const res = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,{
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
+    })
+  
+  const data = await res.json()
+  
+return data
+  }
