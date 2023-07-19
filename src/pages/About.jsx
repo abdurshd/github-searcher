7b72b020edc7f2db27b82fc22b213bd92ff6b640
Abdurashid Akbarov <75764687+inSgit3@users.noreply.github.com>
2023-07-19 16:03:36 +0900

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,28 +2,9 @@
 const About = () => {
   return (
     <>
-    <h1 className='text-6xl mb-4'>Github Finder</h1>
+    <h1 className='text-6xl mb-4'>Github Searcher app</h1>
       <p className='mb-4 text-2xl font-light'>
-        A React app to search GitHub profiles and see profile details. This
-        project is part of the
-        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {' '}
-          React Front To Back
-        </a>{' '}
-        Udemy course by
-        <strong>
-          <a href='https://traversymedia.com'> Brad Traversy</a>
-        </strong>
-        .
-      </p>
-      <p className='text-lg text-gray-400'>
-        Version <span className='text-white'>1.0.0</span>
-      </p>
-      <p className='text-lg text-gray-400'>
-        Layout By: 
-        <a className='text-white' href='https://twitter.com/abdurshd'>
-          Abdurashid Akbarov
-        </a>
+        This is a react app to search GitHub profiles and see profile details. It uses Github's API by using classic generated tokens for free tier of requests. In styling mainly Tailwind CSS has been used. And for it can be improved further by adding some other functionalities as well. 
       </p>
     </>
   )
